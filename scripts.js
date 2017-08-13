4d2316e6cf93cdb46920f756ed0f2f33bcfc212e
jsonberry <jason.s.awbrey@gmail.com>
2017-08-13 16:53:15 -0700

    Finish Part 2

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
-;(function() {
-    console.log('hello jason')
-})()
+window.addEventListener('load', function () {
+    const canvas = document.getElementById("myCanvas")
+    const ctx = canvas.getContext("2d")
+    const dx = 2
+    const dy = -2
+    let x = canvas.width/2;
+    let y = canvas.height-30;
+
+    function drawBall () {
+        ctx.beginPath()
+        ctx.arc(x, y, 10, 0, Math.PI*2)
+        ctx.fillStyle = "#0095DD"
+        ctx.fill()
+        ctx.closePath()
+    }
+
+    function draw () {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        drawBall()
+        x += dx
+        y += dy
+    }
+
+    setInterval(draw, 10)
+})

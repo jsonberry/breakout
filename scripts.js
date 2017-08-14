bb93d6bbcb38c7efc9fc226acfbaf49e0afcebdc
jsonberry <jason.s.awbrey@gmail.com>
2017-08-13 18:36:40 -0700

    Finish Part 4

--- scripts.js
+++ scripts.js
@@ -8,6 +8,11 @@ window.addEventListener('load', function () {
     let dy = -2
     let x = canvas.width/2;
     let y = canvas.height-30;
+    var paddleHeight = 10;
+    var paddleWidth = 75;
+    var paddleX = (canvas.width-paddleWidth)/2;
+    var rightPressed = false;
+    var leftPressed = false;
 
     function drawBall () {
         ctx.beginPath()
@@ -17,9 +22,18 @@ window.addEventListener('load', function () {
         ctx.closePath()
     }
 
+    function drawPaddle () {
+        ctx.beginPath()
+        ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight)
+        ctx.fillStyle = "#0095DD"
+        ctx.fill()
+        ctx.closePath()
+    }
+
     function draw () {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         drawBall()
+        drawPaddle()
 
         if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
             randomColor()
@@ -31,6 +45,13 @@ window.addEventListener('load', function () {
             dy = -dy;
         }
 
+        if (rightPressed && paddleX < canvas.width-paddleWidth) {
+            paddleX += 7;
+        }
+        else if (leftPressed && paddleX > 0) {
+            paddleX -= 7;
+        }
+
         x += dx
         y += dy
     }
@@ -39,5 +60,25 @@ window.addEventListener('load', function () {
         fillStyleColor = _.sample(colors)
     }
 
+    function keyDownHandler(e) {
+        if(e.keyCode == 39) {
+            rightPressed = true;
+        }
+        else if(e.keyCode == 37) {
+            leftPressed = true;
+        }
+    }
+
+    function keyUpHandler(e) {
+        if(e.keyCode == 39) {
+            rightPressed = false;
+        }
+        else if(e.keyCode == 37) {
+            leftPressed = false;
+        }
+    }
+
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
     setInterval(draw, 10)
 })

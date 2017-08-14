0183b173a86cc1224cf38789c97f4d4e28a9695f
jsonberry <jason.s.awbrey@gmail.com>
2017-08-13 17:15:03 -0700

    Finish Part 3

--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
 window.addEventListener('load', function () {
     const canvas = document.getElementById("myCanvas")
     const ctx = canvas.getContext("2d")
-    const dx = 2
-    const dy = -2
+    const ballRadius = 10
+    const colors = ["#0095DD", "#000", "green", "pink"]
+    let fillStyleColor = _.sample(colors)
+    let dx = 2
+    let dy = -2
     let x = canvas.width/2;
     let y = canvas.height-30;
 
     function drawBall () {
         ctx.beginPath()
-        ctx.arc(x, y, 10, 0, Math.PI*2)
-        ctx.fillStyle = "#0095DD"
+        ctx.arc(x, y, ballRadius, 0, Math.PI*2)
+        ctx.fillStyle = fillStyleColor
         ctx.fill()
         ctx.closePath()
     }
@@ -17,8 +20,23 @@ window.addEventListener('load', function () {
     function draw () {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         drawBall()
+
+        if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+            randomColor()
+            dx = -dx;
+        }
+
+        if (y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+            randomColor()
+            dy = -dy;
+        }
+
         x += dx
         y += dy
+    }
+
+    function randomColor () {
+        fillStyleColor = _.sample(colors)
     }
 
     setInterval(draw, 10)
